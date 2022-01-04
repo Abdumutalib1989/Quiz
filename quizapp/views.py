75c9ccf31b6e59ee1ed18d5b89111bb841ad395e
Abdumutalib Mirzajonov <Abdumutalib1989>
2022-01-04 18:07:14 +0500

    templatesda o'zgarish 1

--- quizapp/views.py
+++ quizapp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views import View
+from .models import Quiz, Savol, Javob
 
-# Create your views here.
+class Home(View):
+    def get(self, request):
+        quiz = Quiz.objects.all()
+        return render(request, 'index.html', {"quiz":quiz})
+
+class QuizView(View):
+    def get(self, request, pk):
+        quiz = Quiz.objects.get(id=pk)
+        return render(request, 'quiz.html', {"quiz":quiz})
