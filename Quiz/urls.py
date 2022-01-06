bf3eaed94327223b9375bfb74de2c26b7f326ab9
Abdumutalib Mirzajonov <Abdumutalib1989>
2022-01-06 16:38:42 +0500

    url va view da qo'shildi 1

--- Quiz/urls.py
+++ Quiz/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from quizapp.views import Home, QuizView
+from quizapp.views import Home, QuizView, QuizDataView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', Home.as_view(), name='home'),
     path('<int:pk>/', QuizView.as_view(), name='quiz'),
+    path('<int:pk>/data/', QuizDataView.as_view(), name='quiz-data'),
+    # path('<int:pk>/save/', QuizSaveView.as_view(), name='quiz-save'),
 ]
--- quizapp/views.py
+++ quizapp/views.py
@@ -1,3 +1,4 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 from django.views import View
 from .models import Quiz, Savol, Javob
@@ -11,3 +12,19 @@ class QuizView(View):
     def get(self, request, pk):
         quiz = Quiz.objects.get(id=pk)
         return render(request, 'quiz.html', {"quiz":quiz})
+
+class QuizDataView(View):
+    def get(self, request, pk):
+        quiz = Quiz.objects.get(id=pk)
+        savollar = Savol.objects.filter(quiz=quiz)
+        questions = []
+        for s in savollar:
+            answears = []
+            javoblar = Javob.objects.filter(savol=s)
+            for j in javoblar:
+                answears.append(j.matn)
+            questions.append({str(s):answears})
+        return JsonResponse({
+            'data': questions,
+            'time': quiz.davomiyligi,
+        })
